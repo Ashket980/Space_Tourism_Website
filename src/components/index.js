fdbd55709ca2c024d709964f6780d406cb59cd88
Ashish Kumar <85364791+Ashket980@users.noreply.github.com>
2022-03-01 16:27:43 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header"
+import Home from "./Home"
+import Destination from "./Destination"
+import Crew from "./Crew"
+import Technology from "./Technology"
+import Error from "./Error"
+
+export { Header, Home, Destination, Crew, Technology, Error }
